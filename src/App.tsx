6184514f0e3ccd650e21012b4315e5013bcff67e
Cristian <cristian.ap84@gmail.com>
2025-03-07 10:26:10 -0300

    Added basic app structure.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import './App.scss';
+
+type AppComponent = () => React.ReactNode;
+
+const App: AppComponent = () => {
+  return <div className="app">A web3 payments interface</div>;
+};
+
+export default App;
